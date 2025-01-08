e26cfee7454dd3c0391e9feb962c3ddc34670a7a
Baladevika <131866175+Baladevika@users.noreply.github.com>
2025-01-08 12:56:55 +0530

    Update flame-1-sensor.ino

--- flame-1-sensor.ino
+++ flame-1-sensor.ino
@@ -10,10 +10,10 @@ Serial.begin(9600);
 void loop() {
   if(digitalRead(7)==LOW){
     digitalWrite(8,HIGH);
-    Serial.println("LED ON");
+    Serial.println("Flame Detected");
   }
   else{
     digitalWrite(8,LOW);
-    Serial.println("LED OFF");
+    Serial.println("Falame not detected");
   }
 }
